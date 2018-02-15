4e7845a7d061713514950b337581fe234535a979
oteplova <choobolena@gmail.com>
2018-02-16 01:04:05 +0200

    Add some unit tests (add test for 'MultiTeacher' class)
    #7

---
+++ src/test/java/net/comments/sample/MultiTeacherTest.java
@@ -0,0 +1,14 @@
+package net.comments.sample;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+public class MultiTeacherTest {
+
+    @Test
+    public void testGetName() {
+        MatcherAssert.assertThat(new MultiTeacher("Valentin").getName(), Matchers.is("Valentin"));
+    }
+
+}
