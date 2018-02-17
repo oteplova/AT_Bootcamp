8fe4c39df18e6d4d2de54e197aa9e000805a4353
oteplova <choobolena@gmail.com>
2018-02-17 23:58:58 +0200

    Add some unit tests (updated test for 'TrainingRoomLimited', 'TrainingRoomUnlimited' classes)
    #7

--- src/test/java/net/comments/sample/TrainingRoomLimitedTest.java
+++ src/test/java/net/comments/sample/TrainingRoomLimitedTest.java
@@ -4,22 +4,20 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TrainingRoomLimitedTest {
 
     @Test
     public void testGetEnteredPerson() {
-        List<Person> enteredPerson = new ArrayList<Person>();
         AttentiveStudent student1 = new AttentiveStudent("Student1");
         BadStudent student2 = new BadStudent("Student2");
         MultiTeacher teacher1 = new MultiTeacher("Teacher1");
 
-        enteredPerson.add(student1);
-        enteredPerson.add(student2);
-        enteredPerson.add(teacher1);
+        Room room = new TrainingRoomLimited(3);
 
-        MatcherAssert.assertThat(enteredPerson, IsIterableContainingInOrder.contains(student1, student2, teacher1));
+        room.accept(student1);
+        room.accept(student2);
+        room.accept(teacher1);
+
+        MatcherAssert.assertThat(room.getEnteredPerson(), IsIterableContainingInOrder.contains(student1, student2, teacher1));
     }
 }
