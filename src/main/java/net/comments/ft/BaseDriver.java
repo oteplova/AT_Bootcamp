0f829021145fb8b20024c592439995f1629f19c4
oteplova <choobolena@gmail.com>
2018-02-21 00:33:30 +0200

    Add page objects (update a structure)
    #16

---
+++ src/main/java/net/comments/ft/BaseDriver.java
@@ -0,0 +1,23 @@
+package net.comments.ft;
+
+import net.comments.selenium.CommentsDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseDriver {
+    protected CommentsDriver driver;
+
+    public BaseDriver() {
+        this.driver = new CommentsDriver();
+    }
+
+    @BeforeMethod
+    public void createDriver() {
+        this.driver.defineDriver();
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        //       this.driver.close();
+    }
+}
