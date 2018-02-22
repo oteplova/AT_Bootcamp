3ff35b699a0d0670d083bd4f48bc68653e37eb5d
oteplova <choobolena@gmail.com>
2018-02-22 11:12:59 +0200

    Add page objects (update a test#4)
    #16

--- src/main/java/net/comments/ft/Test4.java
+++ src/main/java/net/comments/ft/Test4.java
@@ -1,24 +1,26 @@
 package net.comments.ft;
 
-import net.comments.objects.BCommentActions;
-import net.comments.objects.BCommentPage;
-import net.comments.objects.CommentActions;
-import net.comments.objects.CommentPage;
+import net.comments.objects.*;
 import org.testng.annotations.Test;
 
 public class Test4 extends BaseDriver {
     private final CommentPage page;
     private final CommentActions action;
+    private final BCommentTable table;
 
     public Test4() {
         this.page = new BCommentPage(this.driver);
         this.action = new BCommentActions(this.driver);
+        this.table = new BCommentTable(this.driver);
     }
 
     @Test
     public void test() {
         page.open();
         action.delete(1);
+        table.setCommentsTable();
+
+
 //
 //        final String page = this.driver().getPageSource();
 //        MatcherAssert.assertThat("Comment Text 0 is present", !page.contains("Comment Text 0"));
--- src/main/java/net/comments/objects/BCommentTable.java
+++ src/main/java/net/comments/objects/BCommentTable.java
@@ -3,8 +3,12 @@ package net.comments.objects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.List;
+
 public class BCommentTable implements CommentTable {
     private final WebDriver driver;
+    private BComment comment;
+    private List<BComment> commentsList;
 
     public BCommentTable(WebDriver driver) {
         this.driver = driver;
@@ -12,5 +16,23 @@ public class BCommentTable implements CommentTable {
 
     public void selectComment(int commentsNumber) {
         this.driver.findElements(By.name("SelectedId")).get(commentsNumber).click();
+    }
+
+    public void setCommentsTable() {
+        String commentNumber;
+        String commentText;
+        String commentActivation;
+        String commentCategories;
+
+        for (int i = 0; i < 10; i++) {
+            this.driver.findElements(By.xpath());
+            commentNumber = "0";
+            commentText = "TTTT";
+            commentActivation = "V";
+            commentCategories = "Cat0";
+            this.comment = new BComment(commentNumber, commentText, commentActivation, commentCategories);
+//            this.commentsList.add(comment);
+        }
+
     }
 }
