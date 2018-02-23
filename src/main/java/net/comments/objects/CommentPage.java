4efc7b30ef807200367637546eb66c8071c302f1
oteplova <choobolena@gmail.com>
2018-02-23 23:21:18 +0200

    Add page object (converted auto test #4)
    #16

--- src/main/java/net/comments/objects/CommentPage.java
+++ src/main/java/net/comments/objects/CommentPage.java
@@ -2,14 +2,9 @@ package net.comments.objects;
 
 public interface CommentPage {
     void open();
-
     void newComment();
-
     CommentActions getCommentAction();
-
     CommentTable getCurrentComments();
-
     CommentTable getCommentsFrom(int page);
-
     int getCommentPageSize();
 }
